2cf2a1731eec9024aa7b7a20d8c938da7dddee83
Cathy97 <sjtujilyy@sjtu.edu.cn>
2018-06-24 11:00:47 +0800

    part4

--- part4.py
+++ part4.py
@@ -1,6 +1,25 @@
 # Imports -- you may add others but do not need to
+import csv
+
+import plotly
 import plotly.plotly as py
 import plotly.graph_objs as go
 
+plotly.tools.set_credentials_file(username='liyuyingcathy', api_key='e6Otvjw0o8OiZ3ZrQWit')
+
 # Code here should involve creation of the bar chart as specified in instructions
 # And opening / using the CSV file you created earlier with noun data from tweets
+x = []
+y = []
+with open('noun_data.csv', 'r') as csv_file:
+    reader = csv.reader(csv_file, delimiter=',', lineterminator='\r\n')
+    for item in reader:
+        if len(item) != 0 and item[0] != 'Noun':
+            x.append(item[0])
+            y.append(item[1])
+    csv_file.close()
+
+data = [go.Bar(x=x, y=y)]
+layout = go.Layout(title='The Most Common Nouns')
+fig = go.Figure(data=data, layout=layout)
+py.image.save_as(fig, filename='part4_viz_image.png')
